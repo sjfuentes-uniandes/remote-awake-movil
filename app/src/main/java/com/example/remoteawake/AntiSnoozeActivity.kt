fe25409446dbdf8602d8a553c4a471d6b727e815
capb90 <capb.dev@gmail.com>
2026-03-12 23:06:25 -0500

    Feat: Creado activity anti snooze

---
+++ app/src/main/java/com/example/remoteawake/AntiSnoozeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.remoteawake
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AntiSnoozeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anti_snooze)
+    }
+}
