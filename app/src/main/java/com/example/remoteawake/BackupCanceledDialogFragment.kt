c1725620e6a18dae467923480bf3d6fa3dca632f
capb90 <capb.dev@gmail.com>
2026-03-12 23:27:15 -0500

    Feat: Creado fragment dialogo de cancelacion

---
+++ app/src/main/java/com/example/remoteawake/BackupCanceledDialogFragment.kt
@@ -0,0 +1,34 @@
+package com.example.remoteawake
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.button.MaterialButton
+
+class BackupCanceledDialogFragment : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.dialog_backup_canceled, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<MaterialButton>(R.id.btnAceptar).setOnClickListener {
+            dismiss()
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        return dialog
+    }
+}
